21a5202a1a822c08b43aacd1352678a1dd32d072
kravchenko89 <symkin2007@gmail.com>
2020-12-24 23:28:46 +0200

    add click on the city

--- main.py
+++ main.py
@@ -1,5 +1,49 @@
 import requests
+import json
 import configparser
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def weather_dashboard():
+    with open('countries.json', 'r') as countries:
+        datas = json.load(countries)
+    return render_template('home.html', datas=datas)
+
+
+@app.route('/results', methods=['POST'])
+def render_results():
+    city_name = request.form['City']
+    api_key = get_api_key()
+    data = get_weather(city_name, api_key)
+    # temp = '{0:.2f}'.format(data['main']['temp'])
+    temp = f"{float(data['main']['temp'])}"
+    feels_like = '{0:.2f}'.format(data['main']['feels_like'])
+    weather = data['weather'][0]['main']
+    location = data['name']
+
+    return render_template('results.html', location=location, temp=temp,
+                           feels_like=feels_like, weather=weather)
+
+
+@app.route('/resultss/<city>/', methods=['GET'])
+def City(city):
+    # import pdb
+    # pdb.set_trace()
+    api_key = get_api_key()
+    data = get_weather(city, api_key)
+    temp = f"{float(data['main']['temp'])}"
+    feels_like = '{0:.2f}'.format(data['main']['feels_like'])
+    weather = data['weather'][0]['main']
+    location = data['name']
+    return render_template('results.html', location=location, temp=temp,
+                           feels_like=feels_like, weather=weather)
+
+
+
 
 
 def get_api_key():
@@ -8,12 +52,12 @@ def get_api_key():
     return config['openweathermap']['api']
 
 
-def get_weather(zip_cod, api_key):
+def get_weather(city_name, api_key):
 
-    api_url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip_cod}&appid={api_key}&mode=json&units=metric"
-    request = requests.get(api_url)
-    print(request)
-    return request.json()
+    api_url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&units=metric&appid={api_key}&mode=json&units=metric"
+    request_q = requests.get(api_url)
+    return request_q.json()
 
 
-print(get_weather('95129', get_api_key()))
+if __name__ == '__main__':
+    app.run()
